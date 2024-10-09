a4723982642de9ab4bfa7e36c9e2ecaa7893836b
permanentct <3307617969@qq.com>
2024-10-09 19:58:08 +0800

    test212

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("cool,帅极了");
+        System.out.println("无敌");
+        System.out.println("cool");
         System.out.println("合并");
         System.out.println("冲突！");
+        System.out.println("key");
     }
 }

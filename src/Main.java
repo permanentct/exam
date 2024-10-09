c081c76bd4ef6f503992fe155d7d312427783284
permanentct <3307617969@qq.com>
2024-10-09 19:52:22 +0800

    test

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("cool");
+        System.out.println("合并");
     }
 }
